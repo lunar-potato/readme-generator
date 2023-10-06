481db421c000da32d48e4b7b84efc20deec547ee
luna potato <gwynethbautistaa@gmail.com>
2023-10-06 18:50:45 +0100

    Added starter code and initialized and installed inquirer

---
+++ index.js
@@ -0,0 +1,20 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const generateReadme = require("./utils/generateReadme");
+
+// Array of questions for user
+const questions = [];
+
+// Function to write README file
+function writeToFile(fileName, data) {
+
+}
+
+// Function to initialize program
+function init() {
+
+}
+
+// Function call to initialize program
+init();
---
+++ utils/generateReadme.js
@@ -0,0 +1,6 @@
+// Function to generate README
+function generateReadme(data) {
+    return `# ${data.title}`;
+}
+
+module.exports = generateReadme;
